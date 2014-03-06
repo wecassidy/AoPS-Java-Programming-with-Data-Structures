64dfa94def581e98887b51dcc6682977ee7c9bad
Tungsten314 <tungsten314@gmail.com>
2014-03-06 08:58:19 -0500

    Added Transcripts

--- Java Files/src/week18/InvertedFile.java
+++ Java Files/src/week18/InvertedFile.java
@@ -1,8 +1,30 @@
 package week18;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class InvertedFile {
-	private HashMap<String, LinkedList<Integer>> table; // The table will contain
+	private HashMap<String, LinkedList<Integer>> table;
+
+	public InvertedFile(String fileName) throws FileNotFoundException {
+		this.table = new HashMap<String, LinkedList<Integer>> ();
+		
+		Scanner s = new Scanner(new File(fileName));
+		
+		s.useDelimiter("*");
+	}
+	
+	public void add(String word, int number) {
+		if (this.table.containsKey(word)) {
+			this.table.get(word).add(number);
+		}
+		
+		else {
+			this.table.put(word, new LinkedList<Integer> ());
+			this.table.get(word).add(number);
+		}
+	}
 }
