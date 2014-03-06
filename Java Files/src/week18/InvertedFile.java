a16ef53b4cbdb6a12372fb650fd1cdf8e54c7d87
Tungsten314 <tungsten314@gmail.com>
2014-03-06 15:43:23 -0500

    Fixed merge problems

--- Java Files/src/week18/InvertedFile.java
+++ Java Files/src/week18/InvertedFile.java
@@ -7,34 +7,24 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class InvertedFile {
-<<<<<<< .merge_file_7Op9Od
 	private HashMap<String, LinkedList<Integer>> words;
 
-	public InvertedFile(String fileName) throws FileNotFoundException {
+public InvertedFile(String fileName) throws FileNotFoundException {
 		this.words = new HashMap<String, LinkedList<Integer>> ();
-=======
-	private HashMap<String, LinkedList<Integer>> table;
-
-	public InvertedFile(String fileName) throws FileNotFoundException {
-		this.table = new HashMap<String, LinkedList<Integer>> ();
->>>>>>> .merge_file_ZZTksu
 		
 		Scanner s = new Scanner(new File(fileName));
 		
 		s.useDelimiter("*");
 	}
-<<<<<<< .merge_file_7Op9Od
-=======
 	
 	public void add(String word, int number) {
-		if (this.table.containsKey(word)) {
-			this.table.get(word).add(number);
+		if (this.words.containsKey(word)) {
+			this.words.get(word).add(number);
 		}
 		
 		else {
-			this.table.put(word, new LinkedList<Integer> ());
-			this.table.get(word).add(number);
+			this.words.put(word, new LinkedList<Integer> ());
+			this.words.get(word).add(number);
 		}
 	}
->>>>>>> .merge_file_ZZTksu
 }
