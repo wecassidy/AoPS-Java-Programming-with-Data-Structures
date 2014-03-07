a3fd293d0dde7fdabc499449d77f2ebc1fccea43
Tungsten314 <tungsten314@gmail.com>
2014-03-06 22:16:33 -0500

    Finished making the inverted file

--- Java Files/src/week18/InvertedFile.java
+++ Java Files/src/week18/InvertedFile.java
@@ -9,22 +9,42 @@ import java.util.Scanner;
 public class InvertedFile {
 	private HashMap<String, LinkedList<Integer>> words;
 
-public InvertedFile(String fileName) throws FileNotFoundException {
+	public InvertedFile(String fileName) throws FileNotFoundException {
 		this.words = new HashMap<String, LinkedList<Integer>> ();
-		
+
 		Scanner s = new Scanner(new File(fileName));
-		
-		s.useDelimiter("*");
+
+		String file = "";
+		for (; s.hasNext(); file += s.nextLine() + "\n");
+
+		String[] docs = file.split("\n\\*\n"); // Split the file into documents by each asterisk (including the newlines before and after it)
+
+		for (int i = 0; i < docs.length; i ++) {
+			for (String word : docs[i].split(" |\n")) { // Go through each word in the current document as split by either a space or a newline
+				this.add(word, i + 1);
+			}
+		}
 	}
-	
+
 	public void add(String word, int number) {
 		if (this.words.containsKey(word)) {
-			this.words.get(word).add(number);
+			if (! this.words.get(word).contains(number)) {
+				this.words.get(word).add(number);
+			}
 		}
-		
+
 		else {
 			this.words.put(word, new LinkedList<Integer> ());
 			this.words.get(word).add(number);
 		}
+	}
+
+	public String toString() {
+		return this.words.toString();
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		InvertedFile f = new InvertedFile("documents.txt");
+		System.out.println(f);
 	}
 }
